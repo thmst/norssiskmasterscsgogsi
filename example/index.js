f8da5fd5e9054dc8bd529e4f42e521f3920ca993
Juho Ärmänen <juho.armanen@hotmail.fi>
2019-11-03 01:01:59 +0200

    1.5

--- example/index.js
+++ example/index.js
@@ -1,40 +1,24 @@
-const fs = require("fs");
 const CSGOGSI = require("../index"); // const CSGOGSI = require("node-csgo-gsi");
-var ks = require('node-key-sender');
-var tickinterval;
 var easymidi = require('easymidi');
-//var virtualInput = new easymidi.Input('CS', true);
-//var virtualOutput = new easymidi.Output('CS', true);
 var output = new easymidi.Output('CS', true);
+
+var pommi = false;
 
 let gsi = new CSGOGSI({
     port: 8080,
     authToken: ["Q79v5tcxVQ8u", "Team2Token", "Team2SubToken"] // this must match the cfg auth token
 });
 
-var pommi = false;
-var purku = false;
-
 
 gsi.on("bombTimeStart", function () {
     console.log("C4 planted");
     pommi = true;
-    while(pommi) {
-        output.send('noteon', {
-        velocity: 127,
-        note: 1,
-        channel: 1
-
-      });
-    }
-    //ks.sendKey('1');
     }
 );
 
 gsi.on("bombState", function (data) {
     if (data === "defused") {
         console.log("defused");
-        //ks.sendKey('2');
     }
 });
 
@@ -42,13 +26,6 @@ gsi.on("roundWinTeam", function (data) {
     if (data === "CT") {
         console.log("CT win");
         pommi = false;
-        output.send('noteon', {
-            note: 2,
-            velocity: 127,
-            channel: 1
-          });
-    
-        //ks.sendKey('3');
     }
 });
 
@@ -56,11 +33,13 @@ gsi.on("roundWinTeam", function (data) {
     if (data === "T") {
         console.log("T win");
         pommi = false;
-        output.send('noteon', {
-            note: 3,
-            velocity: 127,
-            channel: 1
-          });
-        //ks.sendKey('4');
     }
 });
+
+while(pommi) {
+    output.send('noteon', {
+    velocity: 127,
+    note: 1,
+    channel: 1
+  });
+}
